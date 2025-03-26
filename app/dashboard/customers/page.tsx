30f27d71171412d99426a0e99486801e65308939
awul <ilhamnopi@icloud.com>
2025-03-26 19:17:46 +0700

    feat: create Layout and page dashboard

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+const Customer = () => {
+	return (
+		<div>
+			<h1 className="text-2xl font-bold">Customer Pages</h1>
+		</div>
+	);
+};
+
+export default Customer;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+const Invoices = () => {
+	return (
+		<div>
+			<h1 className="text-2xl font-bold">Invoices Pages</h1>
+		</div>
+	);
+};
+
+export default Invoices;
